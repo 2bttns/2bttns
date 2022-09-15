1830850fde0e0eeab4690dbe3e4c8e5c53046d42
Matthew Seto <100644651+mat-2bttns@users.noreply.github.com>
2022-09-14 23:19:18 -0700

    include_list_items query param for GET /api/lists

--- pages/api/lists/index.ts
+++ pages/api/lists/index.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ListModel } from '../../../db'
+import { ListItemModel, ListModel } from '../../../db'
 
 export default async function handler(
     req: NextApiRequest,
@@ -10,8 +10,11 @@ export default async function handler(
         return res.status(405).send({ message: 'Method not allowed' })
     }
 
+    const include_list_items = req.query.include_list_items === 'true'
+    const include = include_list_items ? [ListItemModel] : undefined
+
     try {
-        const result = await ListModel.findAll()
+        const result = await ListModel.findAll({ include })
         return res.status(200).json({
             lists: result.map((l) => {
                 return l.toJSON()
