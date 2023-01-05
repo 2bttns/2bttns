344457cc25de1299c24be15c3f7c2d16410323b3
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-01-05 14:56:25 -0800

    Refactor Games page to Container-View pattern

---
+++ components/pages/games/GamesView.tsx
@@ -0,0 +1,76 @@
+import { Box, Button, Divider, Heading, Stack } from '@chakra-ui/react'
+import Head from 'next/head'
+import Link from 'next/link'
+import { GameAttributes } from '../../../db/models/GameModel'
+import GameForm, { GameFormProps } from './GameForm'
+import GamesTable from './GamesTable'
+
+export type GamesViewProps = {
+    games?: GameAttributes[]
+    handleCreateGame: GameFormProps['onSubmit']
+    handleDeleteGame: (gameId: string) => void
+}
+
+export default function GamesView(props: GamesViewProps) {
+    const { games, handleCreateGame, handleDeleteGame } = props
+    return (
+        <Box sx={{ padding: '1rem', backgroundColor: '#ddd' }}>
+            <Head>
+                <title>My Games</title>
+                <meta name="description" content="My 2bttns Games" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <Box
+                    sx={{
+                        paddingTop: '1rem',
+                        backgroundColor: '#fff',
+                        padding: '1rem',
+                    }}
+                >
+                    <Heading as="h2" size="xl">
+                        Create a Game
+                    </Heading>
+                    <GameForm onSubmit={handleCreateGame} />
+                </Box>
+
+                <Divider orientation="horizontal" sx={{ paddingY: '1rem' }} />
+
+                <Box sx={{ backgroundColor: '#fff', padding: '1rem' }}>
+                    <Heading as="h1" size="2xl">
+                        My Games
+                    </Heading>
+                    {games && (
+                        <GamesTable
+                            games={games}
+                            renderActions={(game) => {
+                                return (
+                                    <Stack direction="row">
+                                        <Link
+                                            href={`/play?id=${game.id}`}
+                                            passHref
+                                        >
+                                            <Button as="a" colorScheme="blue">
+                                                Play
+                                            </Button>
+                                        </Link>
+                                        <Button
+                                            as="a"
+                                            colorScheme="red"
+                                            onClick={() =>
+                                                handleDeleteGame(game.id)
+                                            }
+                                        >
+                                            Delete
+                                        </Button>
+                                    </Stack>
+                                )
+                            }}
+                        />
+                    )}
+                </Box>
+            </main>
+        </Box>
+    )
+}
--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,10 +1,7 @@
-import { Box, Button, Divider, Heading, Stack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
 import { useMutation, useQuery } from 'react-query'
-import GameForm, { GameFormValues } from '../../components/pages/games/GameForm'
-import GamesTable from '../../components/pages/games/GamesTable'
+import { GameFormValues } from '../../components/pages/games/GameForm'
+import GamesView from '../../components/pages/games/GamesView'
 import createGame from '../../lib/api/games/client/createGame'
 import deleteGames from '../../lib/api/games/client/deleteGames'
 import { getGames } from '../../lib/api/games/client/getGames'
@@ -14,9 +11,7 @@ const Games: NextPage = () => {
         return getGames()
     })
     const games = gamesQuery?.data?.games
-    const hasGames = games && games.length > 0
 
-    // React query mutation to create a game
     const { mutate: createGameMutation } = useMutation(
         (values: GameFormValues) => {
             return createGame(values)
@@ -44,69 +39,11 @@ const Games: NextPage = () => {
     }
 
     return (
-        <Box sx={{ padding: '1rem', backgroundColor: '#ddd' }}>
-            <Head>
-                <title>My Games</title>
-                <meta name="description" content="My 2bttns Games" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-                <Box
-                    sx={{
-                        paddingTop: '1rem',
-                        backgroundColor: '#fff',
-                        padding: '1rem',
-                    }}
-                >
-                    <Heading as="h2" size="xl">
-                        Create a Game
-                    </Heading>
-                    <GameForm
-                        onSubmit={(values) => {
-                            console.log('Submitting game', values)
-                            createGameMutation(values)
-                        }}
-                    />
-                </Box>
-
-                <Divider orientation="horizontal" sx={{ paddingY: '1rem' }} />
-
-                <Box sx={{ backgroundColor: '#fff', padding: '1rem' }}>
-                    <Heading as="h1" size="2xl">
-                        My Games
-                    </Heading>
-                    {games && (
-                        <GamesTable
-                            games={games}
-                            renderActions={(game) => {
-                                return (
-                                    <Stack direction="row">
-                                        <Link
-                                            href={`/play?id=${game.id}`}
-                                            passHref
-                                        >
-                                            <Button as="a" colorScheme="blue">
-                                                Play
-                                            </Button>
-                                        </Link>
-                                        <Button
-                                            as="a"
-                                            colorScheme="red"
-                                            onClick={() =>
-                                                handleDeleteGame(game.id)
-                                            }
-                                        >
-                                            Delete
-                                        </Button>
-                                    </Stack>
-                                )
-                            }}
-                        />
-                    )}
-                </Box>
-            </main>
-        </Box>
+        <GamesView
+            games={games}
+            handleCreateGame={createGameMutation}
+            handleDeleteGame={handleDeleteGame}
+        />
     )
 }
 
