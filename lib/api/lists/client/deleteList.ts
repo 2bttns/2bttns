9d0e3ff07d9586f48f190e1812e4d7e5d0a09c8b
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-01-17 12:10:17 -0800

    Update type name

--- lib/api/lists/client/deleteList.ts
+++ lib/api/lists/client/deleteList.ts
@@ -2,11 +2,11 @@ import axios from 'axios'
 import { API_URL } from '../../constants'
 import { DefaultResponse } from './../../constants'
 
-export type DeleteListsParams = {
+export type DeleteListParams = {
     list_id: string
 }
 
-export async function deleteList(params: DeleteListsParams) {
+export async function deleteList(params: DeleteListParams) {
     let url = `${API_URL}/lists/${params.list_id}`
 
     return await axios.delete(url).then((res) => {
