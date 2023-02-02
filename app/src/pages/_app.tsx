4997e5498b2fa3cb494c6dbb4f9f8122a74212de
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-01 19:37:13 -0800

    Set up Chakra UI

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -12,7 +13,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 };
