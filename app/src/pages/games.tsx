54d281a25b28848abb60720dc4dd7cb89688bec3
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-10 17:04:27 -0800

    Delete game

--- app/src/pages/games.tsx
+++ app/src/pages/games.tsx
@@ -1,8 +1,26 @@
+import { DeleteIcon } from "@chakra-ui/icons";
+import { IconButton, Tooltip } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import GamesTable from "../features/games/components/GamesTable";
+import { api, RouterInputs } from "../utils/api";
 
 const GamesPage: NextPage = () => {
+  const utils = api.useContext();
+
+  const deleteGameMutation = api.games.deleteById.useMutation();
+  const handleDeleteGame = async (
+    body: RouterInputs["games"]["deleteById"]
+  ) => {
+    try {
+      await deleteGameMutation.mutateAsync(body);
+      await utils.games.invalidate();
+    } catch (error) {
+      window.alert("Error deleting game\n See console for details");
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -10,7 +28,27 @@ const GamesPage: NextPage = () => {
         <meta name="description" content="Game management panel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GamesTable />
+      <GamesTable
+        additionalActions={(gameData) => {
+          const { id } = gameData;
+          return (
+            <>
+              <Tooltip label={`Delete`} placement="top">
+                <IconButton
+                  colorScheme="red"
+                  onClick={() => {
+                    handleDeleteGame({ id });
+                  }}
+                  icon={<DeleteIcon />}
+                  aria-label={`Delete game with ID: ${id}`}
+                  size="sm"
+                  variant="outline"
+                />
+              </Tooltip>
+            </>
+          );
+        }}
+      />
     </>
   );
 };
