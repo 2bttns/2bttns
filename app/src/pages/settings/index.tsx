4198784bd365e013b4e13251468869d0233820a5
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-15 15:51:03 -0800

    Secret deletion

--- app/src/pages/settings/index.tsx
+++ app/src/pages/settings/index.tsx
@@ -1,17 +1,34 @@
+import { DeleteIcon } from "@chakra-ui/icons";
 import {
   Box,
   Heading,
+  IconButton,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
+  Tooltip,
 } from "@chakra-ui/react";
+import { Secret } from "@prisma/client";
 import SecretsTable from "../../features/settings/containers/SecretsTable";
+import { api } from "../../utils/api";
 
 export type SettingsPage = {};
 
 export default function SettingsPage() {
+  const utils = api.useContext();
+  const deleteSecretMutation = api.secrets.deleteById.useMutation();
+  const handleDeleteSecret = async (id: Secret["id"]) => {
+    try {
+      await deleteSecretMutation.mutateAsync({ id });
+      await utils.secrets.invalidate();
+    } catch (error) {
+      console.error(error);
+      window.alert("Error deleting secret\n See console for details");
+    }
+  };
+
   return (
     <>
       <Heading size="2xl">Settings</Heading>
@@ -25,7 +42,27 @@ export default function SettingsPage() {
           <TabPanels>
             <TabPanel>
               <Box maxHeight="500px" overflow="scroll">
-                <SecretsTable />
+                <SecretsTable
+                  additionalActions={(secretData) => {
+                    const { id } = secretData;
+                    return (
+                      <>
+                        <Tooltip label={`Delete`} placement="top">
+                          <IconButton
+                            colorScheme="red"
+                            onClick={() => {
+                              handleDeleteSecret(id);
+                            }}
+                            icon={<DeleteIcon />}
+                            aria-label={`Delete secret with ID: ${id}`}
+                            size="sm"
+                            variant="outline"
+                          />
+                        </Tooltip>
+                      </>
+                    );
+                  }}
+                />
               </Box>
             </TabPanel>
             <TabPanel>
