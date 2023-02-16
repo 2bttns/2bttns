05a795eb3ec33d74de0dc5e6cf3ee376b8348ada
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-15 18:47:59 -0800

    Fix docs layout

--- app/src/pages/docs/index.tsx
+++ app/src/pages/docs/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
@@ -5,7 +6,11 @@ import "swagger-ui-react/swagger-ui.css";
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 const Home: NextPage = () => {
-  return <SwaggerUI url="/api/openapi.json" />;
+  return (
+    <Box height="100%" overflowY="scroll" bgColor="white" paddingBottom="250px">
+      <SwaggerUI url="/api/openapi.json" />
+    </Box>
+  );
 };
 
 export default Home;
