3aea7c6b9a86c07c259fb77b6f2051f12976c465
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-17 15:23:28 -0800

    Protect /docs

--- app/src/pages/docs/index.tsx
+++ app/src/pages/docs/index.tsx
@@ -1,11 +1,33 @@
 import { Box } from "@chakra-ui/react";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { Session } from "next-auth";
 import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
+import getSessionWithSignInRedirect from "../../utils/getSessionWithSignInRedirect";
 
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
-const Home: NextPage = () => {
+export type DocsPageProps = {
+  session: Session;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { session, redirect } = await getSessionWithSignInRedirect(context);
+
+  if (!session && redirect) {
+    return {
+      redirect,
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
+
+const Home: NextPage<DocsPageProps> = (props) => {
   return (
     <Box height="100%" overflowY="scroll" bgColor="white" paddingBottom="250px">
       <SwaggerUI url="/api/openapi.json" />
