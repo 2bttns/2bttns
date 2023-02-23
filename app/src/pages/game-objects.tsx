7c9b8f6adfba19674cc32c0b441fb95f42dd4a1d
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-02-23 10:37:26 -0800

    Set up EditRelationshipsDrawer

--- app/src/pages/game-objects.tsx
+++ app/src/pages/game-objects.tsx
@@ -1,7 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { LinkIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  IconButton,
+  Input,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
 import type { GetServerSideProps, NextPage } from "next";
 import { Session } from "next-auth";
 import Head from "next/head";
+import React from "react";
 import DeleteGameObjectButton from "../features/gameobjects/containers/DeleteGameObjectButton";
 import GameObjectsTable from "../features/gameobjects/containers/GameObjectsTable";
 import getSessionWithSignInRedirect from "../utils/getSessionWithSignInRedirect";
@@ -38,6 +54,7 @@ const GameObjects: NextPage<GameObjectsPageProps> = (props) => {
         <GameObjectsTable
           additionalActions={(gameObjectData) => (
             <>
+              <EditRelationshipsDrawer />
               <DeleteGameObjectButton gameObjectId={gameObjectData.id} />
             </>
           )}
@@ -48,3 +65,53 @@ const GameObjects: NextPage<GameObjectsPageProps> = (props) => {
 };
 
 export default GameObjects;
+
+function EditRelationshipsDrawer() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef<HTMLButtonElement | null>(null);
+  const title = "Edit Relationships";
+
+  return (
+    <>
+      <Tooltip label={title} placement="top">
+        <IconButton
+          ref={btnRef}
+          colorScheme="teal"
+          onClick={onOpen}
+          aria-label={title}
+          size="sm"
+        >
+          <LinkIcon />
+        </IconButton>
+      </Tooltip>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="xl"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>{title}</DrawerHeader>
+
+          <DrawerBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima
+            cum sapiente maiores, libero inventore nam eum itaque ad beatae
+            incidunt iste perspiciatis nesciunt cupiditate animi quia
+            repellendus veniam cumque iusto! Animi doloremque reiciendis,
+            repellat dolore tenetur voluptas natus quia porro recusandae iure
+            esse ullam eaque assumenda enim rerum odio.
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
