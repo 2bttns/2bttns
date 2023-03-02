475ade290a5180990aa20d96ae7bf21239535551
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-03-02 12:28:53 -0800

    Export formatted gameobjects CSV

--- app/src/features/csv/CsvExport.tsx
+++ app/src/features/csv/CsvExport.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@chakra-ui/react";
 import { Tag } from "@prisma/client";
-import { json2csv } from "json-2-csv";
-import { api } from "../../utils/api";
+import { useCsvExportGameObjects } from "./hooks/useCsvExportGameObjects";
 
 export type CsvExportProps = {
   tagsToExportBy?: Tag["id"][];
@@ -10,57 +9,11 @@ export type CsvExportProps = {
 export default function CsvExport(props: CsvExportProps) {
   const { tagsToExportBy } = props;
 
-  const gameObjectsCount = api.gameObjects.getCount.useQuery({
-    filter: tagsToExportBy
-      ? {
-          tag: {
-            include: tagsToExportBy,
-            exclude: [],
-            includeUntagged: false,
-          },
-        }
-      : undefined,
-  });
-
-  const gameObjectsQuery = api.gameObjects.getAll.useQuery(
-    {
-      take: gameObjectsCount.data?.count,
-      filter: tagsToExportBy
-        ? {
-            tag: {
-              include: tagsToExportBy,
-              exclude: [],
-              includeUntagged: false,
-            },
-          }
-        : undefined,
-      includeTags: true,
-    },
-    {
-      enabled: false,
-      onSuccess: (data) => {
-        if (!data) return;
-        json2csv(data.gameObjects, (err, csv) => {
-          if (err || !csv) return;
-          const blob = new Blob([csv], { type: "text/csv" });
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement("a");
-          link.href = url;
-          link.remove();
-          link.setAttribute("download", "export.csv");
-          link.click();
-        });
-      },
-    }
-  );
-
-  const handleClick = () => {
-    gameObjectsQuery.refetch();
-  };
+  const { handleExport } = useCsvExportGameObjects({ tagsToExportBy });
 
   return (
     <>
-      <Button variant="outline" colorScheme="blue" onClick={handleClick}>
+      <Button variant="outline" colorScheme="blue" onClick={handleExport}>
         Export Csv
       </Button>
     </>
