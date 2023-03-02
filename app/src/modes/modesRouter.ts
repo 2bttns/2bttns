af6bcc173aad2a4713d02facc49ca1a5bdc30c91
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-03-01 19:21:23 -0800

    Upsert gamemode config TRPC procedure

--- app/src/modes/modesRouter.ts
+++ app/src/modes/modesRouter.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter } from "../server/api/trpc";
 import { publicProcedure } from "./../server/api/trpc";
@@ -20,8 +21,84 @@ const resolveMode = publicProcedure
     return { modeUIProps: modeUIConfig };
   });
 
+const upsertGameModeConfig = publicProcedure
+  .input(
+    z.object({
+      gameId: z.string(),
+      mode: z.enum(availableModes),
+      configJson: z.string(),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const { gameId, mode, configJson } = input;
+
+    try {
+      JSON.parse(configJson);
+    } catch (e) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Invalid JSON",
+        cause: e,
+      });
+    }
+
+    try {
+      await ctx.prisma.game.findUniqueOrThrow({
+        where: {
+          id: gameId,
+        },
+      });
+    } catch (e) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Invalid game ID",
+        cause: e,
+      });
+    }
+
+    const gameMode = await ctx.prisma.gameMode.findFirst({
+      where: {
+        gameId: gameId,
+        modeId: mode,
+      },
+    });
+
+    let existingConfig: Object;
+
+    try {
+      existingConfig = gameMode?.modeConfigJson
+        ? JSON.parse(gameMode.modeConfigJson)
+        : {};
+    } catch {
+      existingConfig = {};
+    }
+
+    const nextJsonConfig = JSON.stringify({
+      ...existingConfig,
+      ...JSON.parse(configJson),
+    });
+
+    await ctx.prisma.gameMode.upsert({
+      where: {
+        gameId_modeId: {
+          gameId: gameId,
+          modeId: mode,
+        },
+      },
+      update: {
+        modeConfigJson: nextJsonConfig,
+      },
+      create: {
+        gameId: gameId,
+        modeId: mode,
+        modeConfigJson: nextJsonConfig,
+      },
+    });
+  });
+
 export const modesRouter = createTRPCRouter({
   resolveMode,
+  upsertGameModeConfig,
   classicMode: classicModeRouter,
   // Register additional mode backend routers here
 });
