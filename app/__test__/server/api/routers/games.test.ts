fe521e9fc80a7999f1236fac186d21e6aa2954d9
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-03-16 17:23:46 -0700

    games.getAll test

--- app/__test__/server/api/routers/games.test.ts
+++ app/__test__/server/api/routers/games.test.ts
@@ -10,13 +10,13 @@ import prismaMock from "../../../__mocks__/prismaMock";
 vi.mock("../libs/prisma");
 
 describe("games router", () => {
-  test("games.create should return the generated Game", async () => {
+  test("games.create", async () => {
     const ctx = createInnerTRPCContext({ session: null, prisma: prismaMock });
     const caller = appRouter.createCaller(ctx);
 
     const mockedTime = new Date();
     vi.setSystemTime(mockedTime);
-    const mockGamePrismaOutput: Game = {
+    const mockedOutput: Game = {
       id: "test-game-id",
       name: "test-game",
       description: "test-description",
@@ -26,16 +26,55 @@ describe("games router", () => {
       defaultNumItemsPerRound: 10,
       modeConfigJson: null,
     };
-    prismaMock.game.create.mockResolvedValue(mockGamePrismaOutput);
+    prismaMock.game.create.mockResolvedValue(mockedOutput);
 
     type Input = inferProcedureInput<AppRouter["games"]["create"]>;
     const input: Input = {
-      id: mockGamePrismaOutput.id,
-      name: mockGamePrismaOutput.name,
-      description: mockGamePrismaOutput.description!,
+      id: mockedOutput.id,
+      name: mockedOutput.name,
+      description: mockedOutput.description!,
     };
 
     const result = await caller.games.create(input);
-    expect(result.createdGame).toMatchObject(mockGamePrismaOutput);
+    expect(result.createdGame).toMatchObject(mockedOutput);
+  });
+
+  test("games.getAll", async () => {
+    const ctx = createInnerTRPCContext({ session: null, prisma: prismaMock });
+    const caller = appRouter.createCaller(ctx);
+
+    const mockedTime = new Date();
+    vi.setSystemTime(mockedTime);
+
+    const mockedOutput: Game[] = [
+      {
+        id: "test-game-id",
+        name: "test-game",
+        description: "test-description",
+        mode: defaultMode,
+        createdAt: mockedTime,
+        updatedAt: mockedTime,
+        defaultNumItemsPerRound: 10,
+        modeConfigJson: null,
+      },
+      {
+        id: "test-game-id-2",
+        name: "test-game-2",
+        description: "test-description-2",
+        mode: defaultMode,
+        createdAt: mockedTime,
+        updatedAt: mockedTime,
+        defaultNumItemsPerRound: 5,
+        modeConfigJson: null,
+      },
+    ];
+
+    prismaMock.game.findMany.mockResolvedValue(mockedOutput);
+
+    type Input = inferProcedureInput<AppRouter["games"]["getAll"]>;
+    const input: Input = {};
+
+    const result = await caller.games.getAll(input);
+    expect(result.games).toMatchObject(mockedOutput);
   });
 });
