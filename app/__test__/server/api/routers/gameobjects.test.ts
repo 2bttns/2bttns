5c1013b83b4d7f0fee63a96afceb9629fb2bfe26
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-03-20 13:39:36 -0700

    getRanked test

--- app/__test__/server/api/routers/gameobjects.test.ts
+++ app/__test__/server/api/routers/gameobjects.test.ts
@@ -1,4 +1,5 @@
 // test/sample.test.ts
+import { Tag } from "@prisma/client";
 import { inferProcedureInput } from "@trpc/server";
 import { afterEach, beforeEach, describe, expect, test } from "vitest";
 import { appRouter, AppRouter } from "../../../../src/server/api/root";
@@ -8,10 +9,14 @@ import { prisma } from "../../../../src/server/db";
 describe("gameobjects router", () => {
   beforeEach(async () => {
     await clearGameObjects();
+    await clearTags();
+    await clearPlayers();
   });
 
   afterEach(async () => {
     await clearGameObjects();
+    await clearTags();
+    await clearPlayers();
   });
 
   test("games.create", async () => {
@@ -38,7 +43,7 @@ describe("gameobjects router", () => {
       const caller = appRouter.createCaller(ctx);
 
       const numberOfGames = 101;
-      await createTestGameObjects(numberOfGames);
+      await createTestGameObjects({ count: numberOfGames });
 
       type Input = inferProcedureInput<AppRouter["gameObjects"]["getAll"]>;
       const input: Input = {};
@@ -52,7 +57,7 @@ describe("gameobjects router", () => {
       const caller = appRouter.createCaller(ctx);
 
       const numberOfGameObjects = 101;
-      await createTestGameObjects(numberOfGameObjects);
+      await createTestGameObjects({ count: numberOfGameObjects });
       const testGameObjects = await getAllGameObjects();
       const first10Asc = testGameObjects
         .sort((a, b) => a.name.localeCompare(b.name))
@@ -78,21 +83,87 @@ describe("gameobjects router", () => {
     // TODO: test filters
     // TODO: test other sorting params
   });
+
+  describe("gameobjects.getRanked", async () => {
+    test("getRanked", async () => {
+      const ctx = createInnerTRPCContext({ session: null });
+      const caller = appRouter.createCaller(ctx);
+
+      await prisma.player.create({
+        data: { id: "test-player-id", name: "test-player" },
+      });
+
+      await prisma.tag.createMany({
+        data: [
+          { id: "test-input-tag-id", name: "test-input-tag" },
+          { id: "test-output-tag-id", name: "test-output-tag" },
+        ],
+      });
+
+      const count = 10;
+      await createTestGameObjects({
+        count,
+        tags: ["test-input-tag-id", "test-output-tag-id"],
+      });
+      const testGameObjects = await getAllGameObjects();
+
+      for await (const gameObject of testGameObjects) {
+        await caller.gameObjects.upsertPlayerScore({
+          gameObjectId: gameObject.id,
+          playerId: "test-player-id",
+          score: Math.random(),
+        });
+      }
+
+      const result = await caller.gameObjects.getRanked({
+        playerId: "test-player-id",
+        inputTags: ["test-input-tag-id", "test-output-tag-id"],
+        outputTag: "test-output-tag-id",
+      });
+
+      const expectedSortDesc = [...result.scores].sort(
+        (a, b) => b.score - a.score
+      );
+
+      expect(result.scores).length(count);
+      expect(result.scores).toEqual(expectedSortDesc);
+      expect(result.scores[0]!.score).toEqual(1);
+    });
+  });
 });
 
 async function clearGameObjects() {
   return await prisma.gameObject.deleteMany();
 }
 
-async function createTestGameObjects(count: number) {
-  return await prisma.gameObject.createMany({
+async function createTestGameObjects(options: {
+  count: number;
+  tags?: Tag["id"][];
+}) {
+  const { count, tags = [] } = options;
+  await prisma.gameObject.createMany({
     data: Array.from({ length: count }, (_, i) => ({
       id: `test-gameobject-id-${i}`,
       name: `test-gameobject-${i}`,
     })),
   });
+
+  for await (const gameObject of await getAllGameObjects()) {
+    await prisma.gameObject.update({
+      data: { tags: { connect: tags.map((tagId) => ({ id: tagId })) } },
+      where: { id: gameObject.id },
+    });
+  }
 }
 
 async function getAllGameObjects() {
   return await prisma.gameObject.findMany();
+}
+
+async function clearPlayers() {
+  return await prisma.player.deleteMany();
+}
+
+async function clearTags() {
+  return await prisma.tag.deleteMany();
 }
