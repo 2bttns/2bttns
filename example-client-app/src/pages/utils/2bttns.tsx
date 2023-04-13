4286408a4a2e3dcd7f8b41bdbb4e6c450eed4a5d
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-04-13 10:10:52 -0700

    Example App: Refactor

--- example-client-app/src/pages/utils/2bttns.tsx
+++ example-client-app/src/pages/utils/2bttns.tsx
@@ -1,7 +1,23 @@
-import TwoBttns from "@2bttns/sdk";
+import TwoBttns, { ApiResponses } from "@2bttns/sdk";
 
 export const twobttns = new TwoBttns({
   appId: "example-app",
   secret: "example-secret-value",
   url: "http://localhost:3001",
 });
+
+export const getPlayers = twobttns.api.path("/players").method("get").create();
+export const getTags = twobttns.api.path("/tags").method("get").create();
+export const getRanked = twobttns.api
+  .path("/game-objects/ranked")
+  .method("get")
+  .create();
+
+export type TwoBttnsPlayer =
+  ApiResponses["/players"]["get"]["responses"]["200"]["content"]["application/json"]["players"][number];
+
+export type TwoBttnsTag =
+  ApiResponses["/tags"]["get"]["responses"]["200"]["content"]["application/json"]["tags"][number];
+
+export type TwoBttnsRankedOutput =
+  ApiResponses["/game-objects/ranked"]["get"]["responses"]["200"]["content"]["application/json"];
