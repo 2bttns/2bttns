5deb1d44ef00a3af1af27c479d99d90edfda37b3
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-04-18 17:31:33 -0700

    Token check fixes if user is admin who gets redirected as a player

--- app/src/utils/api.ts
+++ app/src/utils/api.ts
@@ -18,6 +18,36 @@ const getBaseUrl = () => {
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
+let playerToken: string | null = null;
+/**
+ * Setter for `playerToken`.
+ *
+ * When `playerToken` is set, TRPC API calls across the 2bttns admin panel will be authenticated with it.
+ *
+ * This is mainly used in the `/play` page so external players can make (limited) authenticated API calls on the admin panel.
+ *
+ * Note that setting `playerToken` will take precedence over an admin session cookie when making 2bttns API calls.
+ * Be sure to set `playerToken` to `null` when you want to use the admin session cookie again.
+ *
+ * @param newToken The new token to use for authentication
+ */
+export function setPlayerToken(newToken: typeof playerToken) {
+  if (typeof window === "undefined") {
+    throw new Error("setPlayerToken should only be called on the client side");
+  }
+
+  playerToken = newToken;
+  console.log("playerToken set to", playerToken);
+}
+
+function headers() {
+  if (!playerToken) return {};
+
+  return {
+    Authorization: `Bearer ${playerToken}`,
+  };
+}
+
 /**
  * A set of typesafe react-query hooks for your tRPC API
  */
@@ -42,6 +72,7 @@ export const api = createTRPCNext<AppRouter>({
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
+          headers,
         }),
       ],
     };
@@ -72,6 +103,7 @@ export const apiClient = createTRPCProxyClient<AppRouter>({
     }),
     httpBatchLink({
       url: `${getBaseUrl()}/api/trpc`,
+      headers,
     }),
   ],
 });
