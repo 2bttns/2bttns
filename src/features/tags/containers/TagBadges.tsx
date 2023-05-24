b853e15bbf3cf9800899608f62bb938615a6292d
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-05-24 15:25:08 -0700

    TagBadges Show more/Collapse

--- src/features/tags/containers/TagBadges.tsx
+++ src/features/tags/containers/TagBadges.tsx
@@ -1,6 +1,14 @@
-import { Box, Flex, Tag as ChakraTag, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Link as ChakraLink,
+  Tag as ChakraTag,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
 import { Tag } from "@prisma/client";
 import NextLink from "next/link";
+import { useMemo, useState } from "react";
 import { Option } from "react-multi-select-component";
 
 export interface TagOption extends Option {
@@ -10,35 +18,63 @@ export interface TagOption extends Option {
 
 export type TagBadgesProps = {
   selectedTags: { id: Tag["id"]; name: Tag["name"] }[];
+  collapseLetterLimit?: number | "disabled";
 };
 
 export default function TagBadges(props: TagBadgesProps) {
-  const { selectedTags } = props;
+  const { selectedTags, collapseLetterLimit = 32 } = props;
+
+  const shouldCollapse =
+    collapseLetterLimit !== "disabled" &&
+    selectedTags.reduce((acc, { name }) => {
+      return acc + name.length;
+    }, 0) > collapseLetterLimit;
+  const numTagBadgesNonCollapsed = useMemo(() => {
+    if (!shouldCollapse) return selectedTags.length;
+
+    let remainingLetters = collapseLetterLimit;
+    let numTags = 0;
+    selectedTags.forEach(({ name }) => {
+      remainingLetters -= name.length;
+      if (remainingLetters > 0) {
+        numTags++;
+      }
+    });
+    return numTags;
+  }, [shouldCollapse, selectedTags]);
+
+  const numTagBadgesCollapsed = selectedTags.length - numTagBadgesNonCollapsed;
+
+  const [isCollapsed, setCollapsed] = useState(true);
+  const toggleCollapse = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  const tagBadges = selectedTags.map(({ id, name }) => {
+    return (
+      <Tooltip label={`Click to view tag: ${name}`} placement="top" hasArrow>
+        <NextLink href={`/tags/${id}`}>
+          <ChakraTag size="sm" variant="solid" colorScheme={"green"} mr={1}>
+            {name}
+          </ChakraTag>
+        </NextLink>
+      </Tooltip>
+    );
+  });
 
   return (
-    <Box>
+    <VStack width="100%" alignItems="start">
       <Flex flexWrap="wrap" gap="4px">
-        {selectedTags.map(({ id, name }) => {
-          return (
-            <Tooltip
-              label={`Click to view tag: ${name}`}
-              placement="top"
-              hasArrow
-            >
-              <NextLink href={`/tags/${id}`}>
-                <ChakraTag
-                  size="sm"
-                  variant="solid"
-                  colorScheme={"green"}
-                  mr={1}
-                >
-                  {name}
-                </ChakraTag>
-              </NextLink>
-            </Tooltip>
-          );
-        })}
+        {tagBadges.slice(0, numTagBadgesNonCollapsed)}
+        {!isCollapsed && tagBadges.slice(numTagBadgesNonCollapsed)}
       </Flex>
-    </Box>
+      {shouldCollapse && (
+        <Box alignSelf="end">
+          <ChakraLink onClick={toggleCollapse} color="blue.500">
+            {isCollapsed ? `Show ${numTagBadgesCollapsed} More` : "Collapse"}
+          </ChakraLink>
+        </Box>
+      )}
+    </VStack>
   );
 }
