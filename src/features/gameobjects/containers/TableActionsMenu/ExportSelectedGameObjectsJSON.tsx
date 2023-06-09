b589f79a73154bd8eeeb7c6776ce76b2cef29686
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-06-09 12:28:17 -0700

    Refactor export selected game objects to use ExportAllGameObjectsContainer

--- src/features/gameobjects/containers/TableActionsMenu/ExportSelectedGameObjectsJSON.tsx
+++ src/features/gameobjects/containers/TableActionsMenu/ExportSelectedGameObjectsJSON.tsx
@@ -1,5 +1,5 @@
 import { TableActionMenuContext } from "../../../shared/components/Table/containers/TableActionsMenu";
-import TableActionsMenuItemExportJSON from "../../../shared/components/Table/views/TableActionsMenuItemExportJSON";
+import TableActionsMenuItemExportJSONContainer from "../../../shared/components/Table/containers/TableActionsMenu/TableActionsMenuItemExportJSONContainer";
 import { GameObjectData } from "../GameObjectsTable";
 
 export type ExportSelectedGameObjectsJSONProps = {
@@ -11,17 +11,19 @@ export default function ExportSelectedGameObjectsJSON(
 ) {
   const { context } = props;
   return (
-    <TableActionsMenuItemExportJSON
+    <TableActionsMenuItemExportJSONContainer
       context={context}
-      fetchJSON={async (ctx) => {
-        return ctx.selectedRows.map((row) => {
-          return row;
-        });
+      count={(ctx, countData) => {
+        return countData?.gameObjects ?? 0;
       }}
-      menuItemText={(ctx) =>
-        `Export Selected to JSON (${ctx.selectedRows.length})`
-      }
-      isDisabled={(ctx) => ctx.selectedRows.length === 0}
+      exportText={(countValue) => `Export Selected to JSON (${countValue})`}
+      exportDataQueryOptions={{
+        includeCount: true,
+        includeGames: false,
+        includeGameObjects: true,
+        includeTags: true,
+        filterGameObjectIds: context.selectedRows.map((g) => g.id).join(","),
+      }}
     />
   );
 }
