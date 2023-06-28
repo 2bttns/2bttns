380b5ddcb565a15ea9ccc99b0d06abd9442568ac
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-06-28 14:59:59 -0700

    Update tag details editing

--- src/pages/tags/[tagId].tsx
+++ src/pages/tags/[tagId].tsx
@@ -9,8 +9,17 @@ import {
   Heading,
   HStack,
   Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
   Tooltip,
+  Tr,
+  useToast,
+  VStack,
 } from "@chakra-ui/react";
 import { Tag } from "@prisma/client";
 import { GetServerSideProps, NextPage } from "next";
@@ -25,11 +34,13 @@ import GameObjectsTable, {
 import CustomEditable from "../../features/shared/components/CustomEditable";
 import { AdditionalColumns } from "../../features/shared/components/Table/containers/PaginatedTable";
 import TableActionMenu from "../../features/shared/components/Table/containers/TableActionsMenu";
+import UnderlinedTextTooltip from "../../features/shared/components/UnderlinedTextTooltip";
 import DeleteTagButton from "../../features/tags/containers/DeleteTagButton";
 import ToggleTagForSelectedGameObjects from "../../features/tags/containers/TableActionsMenu/ToggleTagForSelectedGameObjects";
 import ToggleTagButton from "../../features/tags/containers/ToggleTagButton";
 import { api, RouterInputs } from "../../utils/api";
 import getSessionWithSignInRedirect from "../../utils/getSessionWithSignInRedirect";
+import wait from "../../utils/wait";
 
 export type TagByIdPageProps = {
   session: Session;
@@ -54,7 +65,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const TagByIdPage: NextPage<TagByIdPageProps> = (props) => {
   const router = useRouter();
   const tagId = router.query.tagId as string;
-
+  const toast = useToast();
   const utils = api.useContext();
 
   const getTagByIdQuery = api.tags.getById.useQuery(
@@ -62,8 +73,8 @@ const TagByIdPage: NextPage<TagByIdPageProps> = (props) => {
     {
       enabled: !!tagId,
       retry: false,
-      onError: (error) => {
-        router.push("/tags");
+      onError: async () => {
+        await router.replace("/tags");
       },
       keepPreviousData: true,
       refetchOnWindowFocus: false,
@@ -72,12 +83,48 @@ const TagByIdPage: NextPage<TagByIdPageProps> = (props) => {
 
   const updateTagMutation = api.tags.updateById.useMutation();
   const handleUpdateTag = async (input: RouterInputs["tags"]["updateById"]) => {
+    let updateDescription = `Saving changes...`;
+    let updateToast = toast({
+      title: "Updating Tag",
+      status: "loading",
+      description: updateDescription,
+    });
     try {
       await updateTagMutation.mutateAsync(input);
-      await utils.tags.invalidate();
+
+      // Redirect to the new tag ID page if the ID changed
+      const id = input.data.id;
+      if (id && id !== tagId) {
+        toast.close(updateToast);
+        updateDescription = `Redirecting to new Tag ID page (${id})...`;
+        updateToast = toast({
+          title: "ID Changed",
+          status: "loading",
+          description: updateDescription,
+        });
+        await wait(1);
+        await router.replace(`/tags/${id}`);
+      }
+
+      updateDescription = ``;
+      toast.update(updateToast, {
+        title: "Saved",
+        status: "success",
+        description: updateDescription,
+      });
+      await utils.tags.getById.invalidate({ id: tagId });
     } catch (error) {
-      window.alert("Error updating tag. See console for details.");
+      updateDescription = `Failed to update (Tag ID=${tagId}). See console for details`;
+      toast.update(updateToast, {
+        title: "Error",
+        status: "error",
+        description: updateDescription,
+      });
+
+      // This will be caught by CustomEditable component using this function
+      // it will revert the value to the previous value when it receives an error
       console.error(error);
+      throw error;
     }
   };
 
@@ -131,6 +178,7 @@ const TagByIdPage: NextPage<TagByIdPageProps> = (props) => {
                 <BreadcrumbLink href={`/tags/${tag.id}`}>
                   {tag.name || "Untitled Tag"}
                   <Text color="blue.500" display="inline">
+                    {" "}
                     ({tag.id})
                   </Text>
                 </BreadcrumbLink>
@@ -142,23 +190,143 @@ const TagByIdPage: NextPage<TagByIdPageProps> = (props) => {
             </ButtonGroup>
           </HStack>
         }
-        <Heading size="xl">
-          <CustomEditable
-            value={tag?.name || ""}
-            placeholder="Untitled Tag"
-            handleSave={async (value) => {
-              await handleUpdateTag({ id: tag.id, data: { name: value } });
-            }}
-          />
-        </Heading>
-        <CustomEditable
-          isTextarea
-          value={tag?.description || ""}
-          placeholder="No description"
-          handleSave={async (value) => {
-            await handleUpdateTag({ id: tag.id, data: { description: value } });
-          }}
-        />
+
+        <Box minW="2xl" maxW="2xl" paddingBottom="5rem">
+          <TableContainer overflowX="visible" overflowY="visible">
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>
+                    <Heading size="md">Tag Details</Heading>
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>
+                    <UnderlinedTextTooltip
+                      tooltipProps={{
+                        label: (
+                          <VStack
+                            spacing={1}
+                            alignItems="start"
+                            fontSize="12px"
+                            padding="1rem"
+                          >
+                            <Text fontWeight="bold">ID</Text>
+                            <Text>
+                              A default ID is generated for you when a Tag is
+                              created.
+                            </Text>
+                            <Text>
+                              An ID may only contain alphanumeric, underscore
+                              (_), and hyphen (-) characters.
+                            </Text>
+                            <Text
+                              color="yellow.500"
+                              fontStyle="bold"
+                              textDecoration="underline"
+                            >
+                              ⚠️ Warning: Changing the ID will change the URL of
+                              the Tag. This may break external references to the
+                              Tag.
+                            </Text>
+                          </VStack>
+                        ),
+                      }}
+                    >
+                      ID
+                    </UnderlinedTextTooltip>
+                  </Td>
+                  <Td>
+                    <CustomEditable
+                      value={tag?.id ?? ""}
+                      placeholder="<Missing ID>"
+                      handleSave={async (value) => {
+                        await handleUpdateTag({
+                          id: tag.id,
+                          data: { id: value },
+                        });
+                      }}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>
+                    <UnderlinedTextTooltip
+                      tooltipProps={{
+                        label: (
+                          <VStack
+                            spacing={1}
+                            alignItems="start"
+                            fontSize="12px"
+                            padding="1rem"
+                          >
+                            <Text fontWeight="bold">NAME</Text>
+                            <Text>
+                              Optional display name of the Game Object.
+                            </Text>
+                          </VStack>
+                        ),
+                      }}
+                    >
+                      Name
+                    </UnderlinedTextTooltip>
+                  </Td>
+                  <Td>
+                    <CustomEditable
+                      value={tag?.name ?? ""}
+                      placeholder="<Untitled Tag>"
+                      handleSave={async (value) => {
+                        await handleUpdateTag({
+                          id: tag.id,
+                          data: { name: value },
+                        });
+                      }}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td verticalAlign="top">
+                    <UnderlinedTextTooltip
+                      tooltipProps={{
+                        label: (
+                          <VStack
+                            spacing={1}
+                            alignItems="start"
+                            fontSize="12px"
+                            padding="1rem"
+                          >
+                            <Text fontWeight="bold">DESCRIPTION</Text>
+                            <Text>
+                              Optional text description of the Game Object.
+                            </Text>
+                          </VStack>
+                        ),
+                      }}
+                    >
+                      Description
+                    </UnderlinedTextTooltip>
+                  </Td>
+                  <Td verticalAlign="top">
+                    <CustomEditable
+                      isTextarea
+                      value={tag?.description ?? ""}
+                      placeholder="<No Description>"
+                      handleSave={async (value) => {
+                        await handleUpdateTag({
+                          id: tag.id,
+                          data: { description: value },
+                        });
+                      }}
+                    />
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+
         <Divider />
 
         <HStack maxWidth="100%" overflow="scroll">
