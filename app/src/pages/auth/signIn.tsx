e53dca9012604fb2213cd0174c37a39afb5d05e3
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-07-10 15:08:55 -0700

    sign in page error handling & toasts

--- app/src/pages/auth/signIn.tsx
+++ app/src/pages/auth/signIn.tsx
@@ -9,6 +9,7 @@ import {
   InputGroup,
   InputLeftAddon,
   Stack,
+  useToast,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import { BuiltInProviderType } from "next-auth/providers";
@@ -19,11 +20,13 @@ import {
   getSession,
   LiteralUnion,
   signIn,
+  SignInResponse,
 } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { FaGithub, FaKey, FaUser } from "react-icons/fa";
+import wait from "../../utils/wait";
 import { NextPageWithLayout } from "../_app";
 
 export type SignInPageProps = {
@@ -60,6 +63,7 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
   const { providers, csrfToken } = props;
 
   const router = useRouter();
+  const toast = useToast();
 
   const filteredProviders = useMemo(() => {
     if (!providers) return null;
@@ -76,6 +80,53 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
   const [credentialsInputStep, setCredentialsInputStep] = useState<
     "username" | "password"
   >("username");
+
+  const [isLoggingIn, setLoggingIn] = useState(false);
+
+  const handleSignIn = useCallback(
+    async (
+      signInMethod: () => Promise<SignInResponse | undefined>,
+      providerName: string | "credentials" = "credentials"
+    ) => {
+      setLoggingIn(true);
+      toast.closeAll();
+      const statusToast = toast({
+        title: "Signing in...",
+        status: "loading",
+      });
+      try {
+        await wait(0.25);
+        const result = await signInMethod();
+        await router.push("/");
+        if (result?.error) {
+          throw new Error(result.error);
+        }
+        let successDescription = `Logged in as: ${credentialsInput.username}`;
+        if (providerName !== "credentials") {
+          successDescription = `Signed in via ${providerName}`;
+        }
+
+        toast.update(statusToast, {
+          title: "Success 👋",
+          description: successDescription,
+          status: "success",
+        });
+      } catch (e) {
+        const errorTitle = `Sign in failed`;
+        const errorDescription = `Username and/or password may be incorrect.`;
+        toast.update(statusToast, {
+          title: errorTitle,
+          description: errorDescription,
+          status: "error",
+        });
+
+        console.error(`${errorTitle}: ${errorDescription}`);
+        console.error(e);
+        setLoggingIn(false);
+      }
+    },
+    [credentialsInput.username, router, toast]
+  );
 
   return (
     <Box width="100vw" height="100vh" overflow="hidden" bgColor="blue.900">
@@ -102,10 +153,23 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                 style={{ margin: "0 auto", userSelect: "none" }}
               />
               <Input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-              <InputGroup fontSize="12px" padding="0">
-                <InputLeftAddon bgColor="gray.200">
-                  {credentialsInputStep === "username" ? <FaUser /> : <FaKey />}
+              <InputGroup
+                fontSize="12px"
+                padding="0"
+                transition="none !important"
+              >
+                <InputLeftAddon
+                  bgColor={isLoggingIn ? "blackAlpha.200" : "gray.200"}
+                  border="none"
+                  transition="none !important"
+                >
+                  {credentialsInputStep === "username" ? (
+                    <FaUser />
+                  ) : (
+                    <FaKey color={isLoggingIn ? "#79746A" : "inherit"} />
+                  )}
                 </InputLeftAddon>
+
                 <Input
                   bgColor="gray.100"
                   type={
@@ -134,6 +198,8 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                       });
                     }
                   }}
+                  isDisabled={isLoggingIn}
+                  transition="none !important"
                 />
               </InputGroup>
               <ButtonGroup width="100%" mt="1rem">
@@ -149,6 +215,8 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                       }
                     }}
                     flex={1}
+                    isDisabled={isLoggingIn}
+                    transition="none !important"
                   >
                     Back
                   </Button>
@@ -161,24 +229,28 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                     }
 
                     if (credentialsInputStep === "password") {
-                      try {
-                        await signIn("credentials", {
-                          username: credentialsInput.username,
-                          password: credentialsInput.password,
-                          redirect: false,
-                        });
-                        await router.push("/");
-                      } catch (e) {
-                        console.error(e);
-                      }
+                      await handleSignIn(
+                        () =>
+                          new Promise(async (res, rej) => {
+                            const result = await signIn("credentials", {
+                              username: credentialsInput.username,
+                              password: credentialsInput.password,
+                              redirect: false,
+                            });
+                            if (!result) return rej();
+                            if (result?.error) return rej(result.error);
+                            res(result);
+                          }),
+                        "credentials"
+                      );
                     }
                   }}
                   flex={1}
                   transition="none !important"
                   isDisabled={
-                    credentialsInputStep === "username"
+                    (credentialsInputStep === "username"
                       ? credentialsInput.username === ""
-                      : credentialsInput.password === ""
+                      : credentialsInput.password === "") || isLoggingIn
                   }
                 >
                   {credentialsInputStep === "username" ? "Next" : "Sign In"}
@@ -191,7 +263,18 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                 filteredProviders.map((provider) => (
                   <div key={provider.name} style={{ marginBottom: 0 }}>
                     <Button
-                      onClick={() => signIn(provider.id, { redirect: false })}
+                      onClick={async () => {
+                        await handleSignIn(
+                          () =>
+                            new Promise(async (res, rej) => {
+                              const result = await signIn(provider.id, {
+                                redirect: false,
+                              });
+                              res(result);
+                            }),
+                          provider.name
+                        );
+                      }}
                       width="100%"
                       bgColor="black"
                       color="white"
@@ -200,6 +283,7 @@ const SignInPage: NextPageWithLayout<SignInPageProps> = (props) => {
                         color: "white",
                       }}
                       rightIcon={getProviderIcon(provider.id) ?? undefined}
+                      isDisabled={isLoggingIn}
                     >
                       Sign in with {provider.name}
                     </Button>
