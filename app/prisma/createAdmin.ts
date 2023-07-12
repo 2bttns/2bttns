170f331ddd11795ae4750f40cf01bbe8a6c4fdd2
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-07-12 15:17:47 -0700

    Add email to allow list via create-admin script

--- app/prisma/createAdmin.ts
+++ app/prisma/createAdmin.ts
@@ -3,16 +3,9 @@ import { PrismaClient } from ".prisma/client";
 const prisma = new PrismaClient();
 
 import inquirer from "inquirer";
-import readline from "readline";
+import { z } from "zod";
 import hashPassword from "../src/utils/hashPassword";
 import { logger } from "../src/utils/logger";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-const question = (prompt: string) =>
-  new Promise<string>((res) => rl.question(prompt, res));
 
 main().catch((e) => {
   if (e instanceof Error) {
@@ -32,62 +25,102 @@ async function main() {
     name: "option",
   });
 
-  if (option === 0) {
-    return;
-  }
-
-  if (option === 1) {
-    const secret = process.env.NEXTAUTH_SECRET;
-    if (!secret)
-      throw new Error(
-        "NEXTAUTH_SECRET is undefined, and is required to for hashing your password. Please set this environment variable. Aborting."
-      );
-
-    const { username } = await inquirer.prompt({
-      type: "input",
-      name: "username",
-    });
-    await validateUsername(username);
-
-    const { password } = await inquirer.prompt({
-      type: "password",
-      name: "password",
-    });
-    const hashedPassword = hashPassword(password, secret);
-
-    try {
-      await prisma.adminCredential.create({
-        data: {
-          hashedPassword,
-          AdminUser: {
-            create: {
-              id: username,
-              displayName: username,
-            },
-          },
-        },
-      });
-      logger.info(
-        `Successfully added admin credentials for ${username} in the database.`
-      );
-    } catch (error) {
-      console.log(error);
-      throw new Error(
-        `Failed to add admin credentials for ${username} in the database. An adminCredential entry already exists for this username.`
-      );
-    }
-    return;
+  switch (option) {
+    case 0:
+      await addOAuthEmailToAdminAllowList();
+      break;
+    case 1:
+      await createAdminWithCredentials();
+      break;
+    default:
+      throw new Error("Invalid option");
   }
 }
+
+async function addOAuthEmailToAdminAllowList() {
+  const { email } = await inquirer.prompt({
+    type: "input",
+    name: "email",
+  });
+  await validateEmail(email);
+  await prisma.adminOAuthAllowList.create({
+    data: {
+      AdminUser: {
+        create: {
+          id: email,
+          displayName: email,
+        },
+      },
+    },
+  });
+
+  logger.info(
+    `Successfully added admin email=${email} to the allow list in the database.`
+  );
+}
+
+async function createAdminWithCredentials() {
+  const secret = process.env.NEXTAUTH_SECRET;
+  if (!secret)
+    throw new Error(
+      "NEXTAUTH_SECRET is undefined, and is required to for hashing your password. Please set this environment variable. Aborting."
+    );
+
+  const { username } = await inquirer.prompt({
+    type: "input",
+    name: "username",
+  });
+  await validateUsername(username);
+
+  const { password } = await inquirer.prompt({
+    type: "password",
+    name: "password",
+  });
+  const hashedPassword = hashPassword(password, secret);
+
+  await prisma.adminCredential.create({
+    data: {
+      hashedPassword,
+      AdminUser: {
+        create: {
+          id: username,
+          displayName: username,
+        },
+      },
+    },
+  });
+  logger.info(
+    `Successfully added admin credentials for ${username} in the database.`
+  );
+}
+
+async function validateEmail(email: string) {
+  if (!email) throw new Error("Email cannot be empty");
+  try {
+    z.string().email().parse(email);
+  } catch {
+    throw new Error(`Invalid email: ${email}`);
+  }
+  const existingAdmin = await prisma.adminOAuthAllowList.findFirst({
+    where: { email },
+  });
+  if (existingAdmin) {
+    throw new Error(
+      `Admin with email=${email} already exists in the database. Aborting.`
+    );
+  }
+  return email;
+}
+
 async function validateUsername(username: string) {
   if (!username) throw new Error("Username cannot be empty");
 
-  const existingUser = await prisma.adminCredential.findFirst({
+  const existingAdmin = await prisma.adminCredential.findFirst({
     where: { username },
   });
-  if (existingUser) {
+  if (existingAdmin) {
     throw new Error(
-      `Username ${username} already exists in the database. Aborting.`
+      `Admin with username=${username} already exists in the database. Aborting.`
     );
   }
   return username;
