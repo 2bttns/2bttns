462e6b2179114566a0ae3ec6dcae9e924eec4bee
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-07-27 12:59:12 -0700

    All or nothing & generate new ID import features

--- app/src/features/shared/components/Table/containers/TableActionsMenu/TableActionsMenuItemImportJSON.tsx
+++ app/src/features/shared/components/Table/containers/TableActionsMenu/TableActionsMenuItemImportJSON.tsx
@@ -1,8 +1,20 @@
-import { Box, MenuItem, Text, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Code,
+  Divider,
+  FormControl,
+  FormLabel,
+  MenuItem,
+  Switch,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import Dropzone from "react-dropzone";
 import { api, apiClient } from "../../../../../../utils/api";
 import ConfirmAlert from "../../../ConfirmAlert";
+import UnderlinedTextTooltip from "../../../UnderlinedTextTooltip";
 import { TableActionMenuContext } from "./index";
 
 export type TableActionsMenuItemImportJSON<T extends object> = {
@@ -21,6 +33,9 @@ export default function TableActionsMenuItemImportJSON<T extends object>(
 
   const [file, setFile] = useState<File | null>(null);
   const [isImporting, setImporting] = useState<boolean>(false);
+
+  const [allOrNothing, setAllOrNothing] = useState<boolean>(false);
+  const [generateNewIds, setGenerateNewIds] = useState<boolean>(false);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length === 0 || !acceptedFiles[0]) {
@@ -41,19 +56,48 @@ export default function TableActionsMenuItemImportJSON<T extends object>(
       const base64 = window.btoa(fileText);
 
       setImporting(true);
+      await utils.importData.invalidate();
       const response = await apiClient.importData.importData.mutate({
         jsonBase64: base64,
+        allOrNothing,
+        generateNewIds,
       });
       await Promise.all([
         utils.tags.invalidate(),
         utils.gameObjects.invalidate(),
         utils.games.invalidate(),
       ]);
-      setImporting(false);
-      // onClose();
+
+      window.alert(
+        `Imported ${response.results.tags.successes} / ${response.results.tags.failures} tags, ${response.results.gameObjects.successes} /
+${response.results.gameObjects.failures} game objects, and ${response.results.games.successes} / ${response.results.games.failures} games.`
+      );
+
+      const errorMessages = response.errorMessages
+        ? `
+=======================================
+${response.errorMessages.join("\n---------------------------------------")}
+=======================================`
+        : "";
+
+      if (allOrNothing && response.allOrNothingFailed) {
+        throw new Error(`
+All or nothing import failed.
+${errorMessages}`);
+      }
+
+      if (response.errorMessages && response.errorMessages.length > 0) {
+        throw new Error(
+          `
+Some objects failed to import. ${response.errorMessages} errors:
+${errorMessages}`
+        );
+      }
     } catch (error) {
       console.error(error);
       window.alert("Error importing data. See console for details.");
+    } finally {
+      setImporting(false);
     }
   };
 
@@ -108,6 +152,75 @@ export default function TableActionsMenuItemImportJSON<T extends object>(
             </Box>
           )}
         </Dropzone>
+
+        <VStack marginTop="1rem">
+          <FormControl
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <FormLabel htmlFor="allOrNothing">
+              <UnderlinedTextTooltip
+                tooltipProps={{
+                  label: (
+                    <Box sx={{ padding: "1rem" }}>
+                      <Text fontWeight="bold">ALL OR NOTHING</Text>
+                      <Divider mt="4px" mb="8px" />
+                      <Text>
+                        When set to <Code>True</Code> : If importing fails for
+                        any reason, the entire import will be cancelled.
+                      </Text>
+                    </Box>
+                  ),
+                }}
+              >
+                All or Nothing:
+              </UnderlinedTextTooltip>
+            </FormLabel>
+            <Switch
+              id="allOrNothing"
+              isChecked={allOrNothing}
+              onChange={(e) => setAllOrNothing(e.target.checked)}
+            />
+          </FormControl>
+
+          <FormControl
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <FormLabel htmlFor="generateNewIds">
+              <UnderlinedTextTooltip
+                tooltipProps={{
+                  label: (
+                    <Box sx={{ padding: "1rem" }}>
+                      <Text fontWeight="bold">GENERATE NEW IDS</Text>
+                      <Divider mt="4px" mb="8px" />
+                      <Text>
+                        When set to <Code>True</Code> : All imported objects
+                        will have a new ID generated for them. This is useful if
+                        you want to import data without risk of ID collisions.
+                      </Text>
+                      <br />
+                      <Text>
+                        However, this may result in duplicate content if you are
+                        importing data that already exists in the database under
+                        different IDs.
+                      </Text>
+                    </Box>
+                  ),
+                }}
+              >
+                Generate New IDs:
+              </UnderlinedTextTooltip>
+            </FormLabel>
+            <Switch
+              id="generateNewIds"
+              isChecked={generateNewIds}
+              onChange={(e) => setGenerateNewIds(e.target.checked)}
+            />
+          </FormControl>
+        </VStack>
       </ConfirmAlert>
       <MenuItem onClick={onOpen}>Import from JSON</MenuItem>
     </>
--- app/src/server/api/routers/import-data/importData.ts
+++ app/src/server/api/routers/import-data/importData.ts
@@ -1,17 +1,40 @@
-import { PrismaPromise } from "@prisma/client";
+import { createId } from "@paralleldrive/cuid2";
+import { Game, GameObject, Prisma, PrismaPromise } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
-import { z } from "zod";
+import { output, z } from "zod";
 import { defaultMode } from "../../../../modes/availableModes";
+import { booleanEnum } from "../../../shared/z";
 import { OPENAPI_TAGS } from "../../openapi/openApiTags";
 import { adminOrApiKeyProtectedProcedure } from "../../trpc";
 import { output as exportDataOutput } from "../export-data/exportData";
 
 const input = z.object({
   jsonBase64: z.string().describe("Base64 encoded JSON file to import"),
+  allOrNothing: booleanEnum
+    .default(false)
+    .describe(
+      "If true, the import will fail if any part of it fails. \n\nIf false, the import will continue even if some parts fail."
+    ),
+  generateNewIds: booleanEnum
+    .default(false)
+    .describe(
+      "Generate new IDs for imported data and remap all references to them, instead of using existing IDs. \n\nThis may result in duplicate entries with similar content but different IDs."
+    ),
+});
+
+const resultCounts = z.object({
+  successes: z.number(),
+  failures: z.number(),
 });
 
 const output = z.object({
-  message: z.string(),
+  results: z.object({
+    tags: resultCounts,
+    gameObjects: resultCounts,
+    games: resultCounts,
+  }),
+  allOrNothingFailed: z.boolean().optional(),
+  errorMessages: z.array(z.string()).optional(),
 });
 
 export const importData = adminOrApiKeyProtectedProcedure
@@ -28,7 +51,7 @@ export const importData = adminOrApiKeyProtectedProcedure
   .input(input)
   .output(output)
   .mutation(async ({ input, ctx }) => {
-    const { jsonBase64 } = input;
+    const { jsonBase64, allOrNothing, generateNewIds } = input;
 
     try {
       const buffer = Buffer.from(jsonBase64, "base64");
@@ -36,87 +59,252 @@ export const importData = adminOrApiKeyProtectedProcedure
       const json = JSON.parse(jsonStr);
       const validated = exportDataOutput.parse(json);
 
-      const queries: PrismaPromise<any>[] = [];
+      const importTimestamp = new Date();
 
+      const remappedIdsOldToNew: {
+        tags: { [id: string]: string };
+        gameObjects: { [id: string]: string };
+        games: { [id: string]: string };
+      } = {
+        tags: {},
+        gameObjects: {},
+        games: {},
+      };
+
+      const remappedIdsNewToOld: typeof remappedIdsOldToNew = {
+        tags: {},
+        gameObjects: {},
+        games: {},
+      };
+
+      const transactionQueries: (
+        | PrismaPromise<Prisma.BatchPayload>
+        | Prisma.Prisma__GameObjectClient<GameObject, never>
+        | Prisma.Prisma__GameClient<Game, never>
+      )[] = [];
+
+      // Create tags
+      let createTagsQuery: PrismaPromise<Prisma.BatchPayload> | undefined;
       if (validated.tags && validated.tags.length > 0) {
-        // Create tags
-        queries.push(
-          ctx.prisma.tag.createMany({
-            data: validated.tags?.map((tag) => ({
-              id: tag.id,
-              name: tag.name,
-            })),
-          })
-        );
+        if (generateNewIds) {
+          validated.tags.forEach((tag) => {
+            const newId = createId();
+            remappedIdsOldToNew.tags[tag.id] = newId;
+            remappedIdsNewToOld.tags[newId] = tag.id;
+          });
+        }
+
+        createTagsQuery = ctx.prisma.tag.createMany({
+          data: validated.tags?.map((tag) => ({
+            id: generateNewIds ? remappedIdsOldToNew.tags[tag.id] : tag.id,
+            name: `${tag.name} (Imported ${importTimestamp.toLocaleString()})`,
+          })),
+        });
+        transactionQueries.push(createTagsQuery);
       }
 
+      // Create game objects
+      let createGameObjectsQuery:
+        | PrismaPromise<Prisma.BatchPayload>
+        | undefined;
+      let applyTagsToGameObjectsQueries:
+        | Prisma.Prisma__GameObjectClient<GameObject, never>[]
+        | undefined;
       if (validated.gameObjects && validated.gameObjects.length > 0) {
-        // Create game objects
-        queries.push(
-          ctx.prisma.gameObject.createMany({
-            data: validated.gameObjects?.map((gameObject) => ({
-              name: gameObject.name,
-              description: gameObject.description,
-              id: gameObject.id,
-            })),
-          })
-        );
+        if (generateNewIds) {
+          validated.gameObjects.forEach((gameObject) => {
+            const newId = createId();
+            remappedIdsOldToNew.gameObjects[gameObject.id] = newId;
+            remappedIdsNewToOld.gameObjects[newId] = gameObject.id;
+          });
+        }
+
+        createGameObjectsQuery = ctx.prisma.gameObject.createMany({
+          data: validated.gameObjects?.map((gameObject) => ({
+            id: generateNewIds
+              ? remappedIdsOldToNew.gameObjects[gameObject.id]
+              : gameObject.id,
+            name: gameObject.name,
+            description: gameObject.description,
+          })),
+        });
+        transactionQueries.push(createGameObjectsQuery);
 
         // Apply imported tags to corresponding game objects
         if (validated.tags && validated.tags.length > 0) {
-          queries.push(
-            ...validated.gameObjects.map((gameObject) => {
+          applyTagsToGameObjectsQueries = validated.gameObjects.map(
+            (gameObject) => {
               return ctx.prisma.gameObject.update({
-                where: { id: gameObject.id },
+                where: {
+                  id: generateNewIds
+                    ? remappedIdsOldToNew.gameObjects[gameObject.id]
+                    : gameObject.id,
+                },
                 data: {
                   tags: {
-                    connect: gameObject.tagIds?.map((id) => ({ id })),
+                    connect: gameObject.tagIds?.map((id) => ({
+                      id: generateNewIds ? remappedIdsOldToNew.tags[id] : id,
+                    })),
                   },
                 },
               });
-            })
+            }
           );
+          transactionQueries.push(...applyTagsToGameObjectsQueries);
         }
       }
 
+      // Create games
+      let createGamesQuery: PrismaPromise<Prisma.BatchPayload> | undefined;
+      let applyTagsToGamesQueries:
+        | Prisma.Prisma__GameClient<Game, never>[]
+        | undefined;
       if (validated.games && validated.games.length > 0) {
+        if (generateNewIds) {
+          validated.games.forEach((game) => {
+            const newId = createId();
+            remappedIdsOldToNew.games[game.id] = newId;
+            remappedIdsNewToOld.games[newId] = game.id;
+          });
+        }
+
         // Create games
-        queries.push(
-          ctx.prisma.game.createMany({
-            data: validated.games?.map((game) => ({
-              id: game.id,
-              name: game.name,
-              description: game.description,
-              mode: defaultMode,
-            })),
-          })
-        );
+        createGamesQuery = ctx.prisma.game.createMany({
+          data: validated.games?.map((game) => ({
+            id: generateNewIds ? remappedIdsOldToNew.games[game.id] : game.id,
+            name: game.name,
+            description: game.description,
+            mode: defaultMode,
+          })),
+        });
+        transactionQueries.push(createGamesQuery);
 
         // Apply imported input tags to corresponding games
         if (validated.tags && validated.tags.length > 0) {
-          queries.push(
-            ...validated.games?.map((game) => {
-              return ctx.prisma.game.update({
-                where: { id: game.id },
-                data: {
-                  inputTags: {
-                    connect: game.inputTagIds?.map((id) => ({ id })),
-                  },
+          applyTagsToGamesQueries = validated.games?.map((game) => {
+            return ctx.prisma.game.update({
+              where: {
+                id: generateNewIds
+                  ? remappedIdsOldToNew.games[game.id]
+                  : game.id,
+              },
+              data: {
+                inputTags: {
+                  connect: game.inputTagIds?.map((id) => ({
+                    id: generateNewIds ? remappedIdsOldToNew.tags[id] : id,
+                  })),
                 },
-              });
-            })
-          );
+              },
+            });
+          });
+          transactionQueries.push(...applyTagsToGamesQueries);
         }
       }
 
-      // All or nothing transaction
-      // TODO: Allow import of partial data
-      await ctx.prisma.$transaction(queries);
+      // Execute the queries
+      const errorMessages: z.infer<typeof output>["errorMessages"] = [];
+      let allOrNothingFailed = undefined;
+      if (allOrNothing) {
+        // If allOrNothing=true, execute all queries in a transaction
+        try {
+          await ctx.prisma.$transaction(transactionQueries);
+          allOrNothingFailed = false;
+        } catch (e) {
+          allOrNothingFailed = true;
+          if (e instanceof Error) {
+            errorMessages.push(e.message);
+          }
+        }
+      } else {
+        // Execute all queries, but continue even if there are errors for allOrNothing=false
+        const takeErrorMessages = (e: Error) => {
+          errorMessages.push(e.message);
+        };
+        await Promise.all(
+          [createTagsQuery].map((q) => q?.catch(takeErrorMessages))
+        );
+        await Promise.all(
+          [createGameObjectsQuery].map((q) => q?.catch(takeErrorMessages))
+        );
+        await Promise.all(
+          [createGamesQuery].map((q) => q?.catch(takeErrorMessages))
+        );
+        if (applyTagsToGameObjectsQueries)
+          await Promise.all(
+            applyTagsToGameObjectsQueries.map((q) => q.catch(takeErrorMessages))
+          );
+        if (applyTagsToGamesQueries)
+          await Promise.all(
+            applyTagsToGamesQueries.map((q) => q.catch(takeErrorMessages))
+          );
+      }
+
+      const importedTags = await ctx.prisma.tag.findMany({
+        where: {
+          id: {
+            in: validated.tags?.map((tag) =>
+              generateNewIds ? remappedIdsOldToNew.tags[tag.id]! : tag.id
+            ),
+          },
+          createdAt: {
+            gte: importTimestamp,
+          },
+        },
+      });
+
+      const importedGameObjects = await ctx.prisma.gameObject.findMany({
+        where: {
+          id: {
+            in: validated.gameObjects?.map((gameObject) =>
+              generateNewIds
+                ? remappedIdsOldToNew.gameObjects[gameObject.id]!
+                : gameObject.id
+            ),
+          },
+          createdAt: {
+            gte: importTimestamp,
+          },
+        },
+      });
+
+      const importedGames = await ctx.prisma.game.findMany({
+        where: {
+          id: {
+            in: validated.games?.map((game) =>
+              generateNewIds ? remappedIdsOldToNew.games[game.id]! : game.id
+            ),
+          },
+          createdAt: {
+            gte: importTimestamp,
+          },
+        },
+      });
+
+      const results: z.infer<typeof output>["results"] = {
+        tags: {
+          successes: importedTags.length,
+          failures: validated.tags
+            ? validated.tags.length - importedTags.length
+            : 0,
+        },
+        gameObjects: {
+          successes: importedGameObjects.length,
+          failures: validated.gameObjects
+            ? validated.gameObjects.length - importedGameObjects.length
+            : 0,
+        },
+        games: {
+          successes: importedGames.length,
+          failures: validated.games
+            ? validated.games.length - importedGames.length
+            : 0,
+        },
+      };
 
       return {
-        message: `Successfully imported ${validated.count?.tags ?? 0} tag(s), ${
-          validated.count?.games ?? 0
-        } game(s), and ${validated.count?.gameObjects ?? 0} game object(s).`,
+        results,
+        allOrNothingFailed,
+        errorMessages,
       };
     } catch (error) {
       throw new TRPCError({
