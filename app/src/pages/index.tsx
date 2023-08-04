40e6018b13a357c429eda4aa8c6bb570724a865a
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-03 18:03:49 -0700

    Working React Joyride tutorial setup

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps, type NextPage } from "next";
 import { Session } from "next-auth";
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import {
   FaBookOpen,
   FaGamepad,
@@ -12,7 +12,12 @@ import {
   FaShapes,
   FaTags,
 } from "react-icons/fa";
-import type ReactJoyride from "react-joyride";
+import ReactJoyride, {
+  ACTIONS as JOYRIDE_ACTIONS,
+  CallBackProps as JoyrideCallBackProps,
+  EVENTS as JOYRIDE_EVENTS,
+  STATUS as JOYRIDE_STATUS,
+} from "react-joyride";
 import PreviewLinkCard from "../features/shared/components/PreviewLinkCard";
 import getSessionWithSignInRedirect from "../utils/getSessionWithSignInRedirect";
 const ReactJoyrideComponent = dynamic(() => import("react-joyride"), {
@@ -40,7 +45,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const Home: NextPage<HomePageProps> = (props) => {
-  const [steps, setSteps] = useState<ReactJoyride["props"]>({
+  const [joyride, setJoyride] = useState<ReactJoyride["props"]>({
+    run: false,
     steps: [
       {
         target: "#manage-games-card",
@@ -53,12 +59,29 @@ const Home: NextPage<HomePageProps> = (props) => {
         disableBeacon: true,
       },
     ],
-    run: false,
-    debug: true,
-    showProgress: true,
-    continuous: true,
-    showSkipButton: true,
+    stepIndex: 0,
   });
+
+  const handleJoyrideCallback = useCallback((data: JoyrideCallBackProps) => {
+    const { action, index, status, type } = data;
+
+    if (
+      [JOYRIDE_EVENTS.STEP_AFTER, JOYRIDE_EVENTS.TARGET_NOT_FOUND].includes(
+        type as any
+      )
+    ) {
+      // Update state to advance the tour
+      setJoyride((prev) => ({
+        ...prev,
+        stepIndex: index + (action === JOYRIDE_ACTIONS.PREV ? -1 : 1),
+      }));
+    } else if (
+      [JOYRIDE_STATUS.FINISHED, JOYRIDE_STATUS.SKIPPED].includes(status as any)
+    ) {
+      // Need to set our running state to false, so we can restart if we click start again.
+      setJoyride((prev) => ({ ...prev, run: false }));
+    }
+  }, []);
 
   return (
     <>
@@ -72,14 +95,24 @@ const Home: NextPage<HomePageProps> = (props) => {
           <Tooltip label="View tutorial for this page" placement="left">
             <IconButton
               onClick={() => {
-                setSteps({ ...steps, run: true });
+                setJoyride((prev) => ({ ...prev, stepIndex: 0, run: true }));
               }}
               icon={<FaQuestionCircle />}
               aria-label="View tutorial for this page"
             />
           </Tooltip>
         </Box>
-        <ReactJoyrideComponent {...steps} />
+        <ReactJoyrideComponent
+          {...joyride}
+          callback={handleJoyrideCallback}
+          showProgress
+          continuous
+          showSkipButton
+          disableScrolling
+          disableOverlayClose
+          disableCloseOnEsc
+          hideCloseButton
+        />
         <VStack
           alignItems="center"
           height="100%"
