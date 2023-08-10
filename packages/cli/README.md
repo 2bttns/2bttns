01d9a4b3ad7454ea96773ab03f8d16a21f34b5a0
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-10 13:48:15 -0700

    Set up CLI

---
+++ packages/cli/README.md
@@ -0,0 +1,13 @@
+# create-2bttns-app
+
+Create your 2bttns app with a single command.
+
+## Usage
+
+```bash
+# Create a new 2bttns app in a new / existing directory
+$ npx @2bttns/create-2bttns-app <your-2bttns-app-name>
+
+# Create in the current directory
+$ npx @2bttns/create-2bttns-app .
+```
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { program } from "commander";
+
+program
+  .name("@2bttns/cli")
+  .description("The 2bttns command line utility")
+  .version("0.0.1-alpha");
+
+const brew = program.command("db");
+brew.command("migrate").action(() => {
+  console.log("migrate DB");
+});
+brew.command("seed").action(() => {
+  console.log("seed DB");
+});
+
+program.parse(process.argv);
