c895ccccc5f44938b49108387e4bddb9f20e4fbf
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-10 15:12:19 -0700

    Set up commander + connect to DB via main app prisma schema

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { Option, program } from "commander";
+import { PrismaClient } from "../../../app/node_modules/@prisma/client";
 
 program
   .name("@2bttns/cli")
@@ -7,11 +8,33 @@ program
   .version("0.0.1-alpha");
 
 const db = program.command("db");
-db.command("migrate").action(() => {
+
+db.addOption(new Option("-d, --db-url <database>", "database connection url"));
+db.command("migrate").action(async (name, options, command) => {
+  const dbUrl = options.parent._optionValues.dbUrl;
+  if (!dbUrl) throw new Error("dbUrl is required");
+  process.env.DATABASE_URL = dbUrl;
+  const prisma = new PrismaClient();
+  await checkDbConnection(prisma);
   console.log("migrate DB");
 });
-db.command("seed").action(() => {
+db.command("seed").action(async (name, options, command) => {
+  const dbUrl = options.parent._optionValues.dbUrl;
+  if (!dbUrl) throw new Error("dbUrl is required");
+  process.env.DATABASE_URL = dbUrl;
+  const prisma = new PrismaClient();
+  await checkDbConnection(prisma);
   console.log("seed DB");
 });
+
+async function checkDbConnection(prisma: PrismaClient) {
+  try {
+    await prisma.$connect();
+  } catch (e) {
+    console.error("Error connecting to DB");
+    console.error(e);
+    process.exit(1);
+  }
+}
 
 program.parse(process.argv);
