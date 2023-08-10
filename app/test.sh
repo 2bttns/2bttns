1475316d108b28d0d06b4a24acc5f137d0ab35af
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-10 13:48:11 -0700

    Update

--- app/test.sh
+++ app/test.sh
@@ -1,4 +1,4 @@
 docker container run \
-    --name 2bttns -p 80:3262 --rm  \
+    --name 2bttns -p 3262:3262 --rm  \
     -e DATABASE_URL="postgresql://dev-user:dev-pass@host.docker.internal:5433/dev-db" \
     2bttns/2bttns
