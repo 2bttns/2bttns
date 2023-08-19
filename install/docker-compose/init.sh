1670d1742d0ba358b3eac302a37b708b48226861
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-18 17:17:22 -0700

    WIP

--- install/docker-compose/init.sh
+++ install/docker-compose/init.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-echo "TEST"
-
 # This value should match the docker-compose service name of your 2bttns container
 TWOBTTNS_SERVICE_NAME=twobttns
 POSTGRES_SERVICE_NAME=db
 
-echo "After vars set"
-
 # Start your containers via docker-compose
 docker-compose up -d 
-
-echo "After compose up"
 
 # Apply the necessary 2bttns Prisma migrations to the database
 docker-compose exec -T $TWOBTTNS_SERVICE_NAME 2bttns-cli db migrate
 
 # Seed the database with example data (optional)
 docker-compose exec -T $TWOBTTNS_SERVICE_NAME 2bttns-cli db seed
-
-echo "After execs"
 
 # Ensure the postgresql service is running
 if [ -z "$(docker-compose ps -q $POSTGRES_SERVICE_NAME)" ]; then
@@ -31,10 +23,6 @@ if [ -z "$(docker-compose ps -q $TWOBTTNS_SERVICE_NAME)" ]; then
     echo "'$TWOBTTNS_SERVICE_NAME' Docker service failed to start. Exiting..."
     exit 1
 fi
-
-echo "After checks"
-
-sleep 1
 
 # Echo the port of the twobttns container
 echo ""
