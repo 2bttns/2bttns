39a6f7d5ce7bcbf489acb52aa9f10c3b0a5a22f2
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-18 17:19:43 -0700

    WIP

--- install/docker-compose/init.sh
+++ install/docker-compose/init.sh
@@ -30,6 +30,7 @@
 # output="${output#*:}"
 # echo "Your 2bttns instance is now running at port :$output"
 
+
 echo ""
 echo "To log in to your 2bttns console, you'll need to create an admin user account."
 echo "You can do this by running \`docker-compose exec $TWOBTTNS_SERVICE_NAME 2bttns-cli admin create\`"
