ef0fe5accfe8d3f2c336f6f1f781d62daa9c35a2
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2023-08-31 12:33:27 -0700

    CopyTextButton + add to Game ID cell

---
+++ app/src/features/shared/components/CopyTextButton.tsx
@@ -0,0 +1,60 @@
+import {
+  ButtonProps,
+  IconButton,
+  Tooltip,
+  TooltipProps,
+  useToast,
+} from "@chakra-ui/react";
+import { FaCopy } from "react-icons/fa";
+
+export type CopyTextButtonProps = {
+  textToCopy: string;
+  tooltipLabel?: string;
+  tooltipProps?: TooltipProps;
+  buttonProps?: ButtonProps;
+};
+
+/**
+ * Button that copies the specified text to the clipboard
+ */
+export default function CopyTextButton(props: CopyTextButtonProps) {
+  const {
+    tooltipLabel = "Copy to clipboard",
+    textToCopy,
+    tooltipProps,
+    buttonProps,
+  } = props;
+
+  const toast = useToast();
+
+  const handleCopy = async () => {
+    toast.closeAll();
+    if (!navigator.clipboard) {
+      console.error("Clipboard API not available");
+      toast({
+        title: "Copy failed: Clipboard API not available",
+        status: "error",
+      });
+      return;
+    }
+    await navigator.clipboard.writeText(textToCopy);
+    toast({
+      title: "Copied to clipboard",
+      status: "success",
+    });
+  };
+
+  return (
+    <Tooltip label={tooltipLabel} placement="top" {...tooltipProps}>
+      <IconButton
+        aria-label="Copy to clipboard"
+        icon={<FaCopy />}
+        onClick={handleCopy}
+        size="sm"
+        variant="ghost"
+        colorScheme="blue"
+        {...buttonProps}
+      />
+    </Tooltip>
+  );
+}
