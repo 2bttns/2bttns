376a398189e3ae4c504e4bccb20050e3365a014b
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2024-02-20 21:46:22 -0800

    expose tags getById

--- app/src/server/api/routers/tags/getById.ts
+++ app/src/server/api/routers/tags/getById.ts
@@ -1,13 +1,35 @@
 import { z } from "zod";
 import { idSchema } from "../../../shared/z";
+import { OPENAPI_TAGS } from "../../openapi/openApiTags";
 import { adminOrApiKeyProtectedProcedure } from "../../trpc";
 
 const input = z.object({
   id: idSchema,
 });
 
+const output = z.object({
+  tag: z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string().nullable(),
+    createdAt: z.string().describe("ISO date string"),
+    updatedAt: z.string().describe("ISO date string"),
+  }),
+});
+
 export const getById = adminOrApiKeyProtectedProcedure
+  .meta({
+    openapi: {
+      summary: "Get Tag by ID",
+      description: "Get a Tag by its ID.",
+      tags: [OPENAPI_TAGS.TAGS],
+      method: "GET",
+      path: "/tags/{id}",
+      protect: true,
+    },
+  })
   .input(input)
+  .output(output)
   .query(async ({ ctx, input }) => {
     const tag = await ctx.prisma.tag.findFirst({
       where: {
@@ -19,5 +41,13 @@ export const getById = adminOrApiKeyProtectedProcedure
       throw new Error(`Tag with id='${input.id}' not found`);
     }
 
-    return { tag };
+    return {
+      tag: {
+        id: tag.id,
+        name: tag.name,
+        description: tag.description,
+        createdAt: tag.createdAt.toISOString(),
+        updatedAt: tag.updatedAt.toISOString(),
+      },
+    };
   });
