135877d2bd6685e7fb57a515a50166e31b4576df
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2024-02-23 21:26:22 -0800

    Expose endpoint to update tag by ID

--- app/src/server/api/routers/tags/updateById.ts
+++ app/src/server/api/routers/tags/updateById.ts
@@ -1,5 +1,7 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { idSchema } from "../../../shared/z";
+import { OPENAPI_TAGS } from "../../openapi/openApiTags";
 import { adminOrApiKeyProtectedProcedure } from "../../trpc";
 
 const input = z.object({
@@ -13,53 +15,98 @@ const input = z.object({
   }),
 });
 
-export const updateById = adminOrApiKeyProtectedProcedure
-  .input(input)
-  .mutation(async ({ ctx, input }) => {
-    const updatedTag = await ctx.prisma.tag.update({
-      where: {
-        id: input.id,
-      },
-      data: {
-        id: input.data.id,
-        name: input.data.name,
-        description: input.data.description,
-        gameObjects: {
-          connect: input.data.addGameObjects
-            ? input.data.addGameObjects.map((id) => ({ id }))
-            : undefined,
-          disconnect: input.data.removeGameObjects
-            ? input.data.removeGameObjects.map((id) => ({ id }))
-            : undefined,
-        },
-        updatedAt: new Date(),
-      },
-    });
+const output = z.object({
+  updatedTag: z.object({
+    id: z.string(),
+    createdAt: z.string().describe("ISO date string"),
+    updatedAt: z.string().describe("ISO date string"),
+    name: z.string(),
+    description: z.string().nullable(),
+    gameObjectCount: z.number(),
+  }),
+});
 
-    // Update updatedAt timestamp on all gameObjects that were connected or disconnected
-    if (input.data.addGameObjects || input.data.removeGameObjects) {
-      await ctx.prisma.gameObject.updateMany({
+export const updateById = adminOrApiKeyProtectedProcedure
+  .meta({
+    openapi: {
+      summary: "Update Tag by ID",
+      description: "Update a Tag by its ID",
+      tags: [OPENAPI_TAGS.TAGS],
+      method: "PUT",
+      path: "/tags/{id}",
+      protect: true,
+    },
+  })
+  .input(input)
+  .output(output)
+  .mutation(async ({ ctx, input }) => {
+    try {
+      const updatedTag = await ctx.prisma.tag.update({
         where: {
-          OR: [
-            {
-              id: {
-                in: input.data.addGameObjects ?? [],
-              },
-            },
-            {
-              id: {
-                in: input.data.removeGameObjects ?? [],
-              },
-            },
-          ],
+          id: input.id,
         },
         data: {
+          id: input.data.id,
+          name: input.data.name,
+          description: input.data.description,
+          gameObjects: {
+            connect: input.data.addGameObjects
+              ? input.data.addGameObjects.map((id) => ({ id }))
+              : undefined,
+            disconnect: input.data.removeGameObjects
+              ? input.data.removeGameObjects.map((id) => ({ id }))
+              : undefined,
+          },
           updatedAt: new Date(),
         },
       });
-    }
 
-    return {
-      updatedTag,
-    };
+      const gameObjectCount = await ctx.prisma.gameObject.count({
+        where: {
+          tags: {
+            some: {
+              id: input.id,
+            },
+          },
+        },
+      });
+
+      // Update updatedAt timestamp on all gameObjects that were connected or disconnected
+      if (input.data.addGameObjects || input.data.removeGameObjects) {
+        await ctx.prisma.gameObject.updateMany({
+          where: {
+            OR: [
+              {
+                id: {
+                  in: input.data.addGameObjects ?? [],
+                },
+              },
+              {
+                id: {
+                  in: input.data.removeGameObjects ?? [],
+                },
+              },
+            ],
+          },
+          data: {
+            updatedAt: new Date(),
+          },
+        });
+      }
+
+      return {
+        updatedTag: {
+          ...updatedTag,
+          createdAt: updatedTag.createdAt.toISOString(),
+          updatedAt: updatedTag.updatedAt.toISOString(),
+          gameObjectCount: gameObjectCount ?? 0,
+        },
+      };
+    } catch (error) {
+      console.error(error);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
   });
